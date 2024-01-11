46f7210a9a41bf791f2cb1e697d90250eb2348cc
Christopher Hannah <me@chrishannah.me>
2024-01-11 00:20:13 +0000

    feat: update telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,12 +2,7 @@ local actions = require "telescope.actions"
 local action_layout = require("telescope.actions.layout")
 require('telescope').setup{
     defaults = {
-        layout_strategy = "vertical",
-        layout_config = {
-            vertical = {
-                width = 0.5
-            },
-        },
+        layout_strategy = "horizontal",
         mappings = {
             i = {
                 ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
